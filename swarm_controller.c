863979d29385384d9bfe38ba015c21998c3f007c
Pi <paingthetko.pi@gmail.com>
2020-11-14 22:29:50 +0630

    Update swarm_controller.c

--- swarm_controller.c
+++ swarm_controller.c
@@ -1,29 +1,26 @@
 /*
- * Copyright 1996-2020 Cyberbotics Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Simple Swarm Robot Controller
+ * Maryam and Pi
+ * 
+
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <webots/distance_sensor.h>
 #include <webots/led.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
-
+#include <webots/keyboard.h>
 #include <webots/gps.h>
 
+#define PI 3.141592
+#define RADIUS 1
 
+#define DELTA 10
+
+#define MAX_SPEED 10
 
 #define TIME_STEP 64
 #define RANGE (127 / 2)
@@ -56,6 +53,25 @@ static void init_devices() {
   step();
 }
 
+
+/* get x coordinate destination*/
+double get_xDestination(int N,int n) {
+
+   /* local variable declaration */
+   double result;
+   result = RADIUS*cos((2*PI*n)/(N));
+   return result; 
+}
+/* get x coordinate destination*/
+double get_yDestination(int N,int n) {
+
+   /* local variable declaration */
+   double result;
+   result = RADIUS*sin((2*PI*n)/(N));
+   return result; 
+}
+
+
 int main(int argc, char *argv[]) {
   /* define variables */
   long int currentColor = 0;
@@ -64,10 +80,25 @@ int main(int argc, char *argv[]) {
   WbDeviceTag rgbLed;
   WbDeviceTag distance_sensor[8];
   WbDeviceTag left_motor, right_motor;
-  int i, j;
-  double speed[2] = {0, 0};
+  int i;
+  double speed_left = 0;
+  double speed_right = 0;
   double sensors_value[8];
-  double braitenberg_coefficients[8][2] = {{10, 8}, {7, -1.5}, {5, -1}, {-1, -1}, {-1, -1}, {-1, -1}, {-1, 5}, {-1.5, 7}};
+
+  /*Variables for destination determination*/
+  int N,n,delta;
+  double xD,yD,y0,x0,x,y;
+  delta = 0;
+  N = 0;
+  n = 1;
+  xD = 0;
+  yD = 0;
+  y0 = 0;
+  x0 = 0;
+  x=0;
+  y=0;
+  
+  bool simulation_started = false;
 
   /* initialize Webots */
   wb_robot_init();
@@ -108,36 +139,144 @@ int main(int argc, char *argv[]) {
   WbDeviceTag gps = wb_robot_get_device("gps");
   wb_gps_enable(gps, TIME_STEP);
   
+   /* get key presses from keyboard */
+  wb_keyboard_enable(TIME_STEP);
+
+  /*Welcome Message*/
+  printf("Welcome to Simple Swarm Formation\n");
+  printf("Please run the simulation and enter a number between 3 to 9.\n");
+  
 
   /* main loop */
   while (true) {
-    /* get sensors values */
-    for (i = 0; i < 8; i++)
-      sensors_value[i] = wb_distance_sensor_get_value(distance_sensor[i]);
+  
+  if(delta > DELTA){
+    delta = 0;
+    }
+    
+    /*Get input from user*/
+    switch (wb_keyboard_get_key()) {
+      case '3': {
+        N=3;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '4': {
+        N=4;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '5': {
+        N=5;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '6': {
+        N=6;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '7': {
+        N=7;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '8': {
+        N=8;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
+      case '9': {
+        N=9;
+        xD = get_xDestination(N,n);
+        yD = get_yDestination(N,n);
+        simulation_started = true;
+        break;
+      }
 
-    /*
+      default:
+        break;
+    }
+    
+    if(!simulation_started && N != 0){
+      printf("Forming %d-sided polygon. \n",N);
+    }
+    
+    if(simulation_started){
+    
+    
+    
+    /* get sensors values */
+    for (i = 0; i < 8; i++){
+      sensors_value[i] = wb_distance_sensor_get_value(distance_sensor[i]);
+     }
+      
+      
      printf("prox values = %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f %0.2f\n",
      sensors_value[0], sensors_value[1], sensors_value[2], sensors_value[3],
      sensors_value[4], sensors_value[5], sensors_value[6], sensors_value[7]);
-    */
+    
     
     const double *gps_values = wb_gps_get_values(gps);
-    const double x_coordinate = gps_values[0];
-    const double y_coordinate = gps_values[2];
-    printf("Using the GPS device: %.3f %.3f\n", x_coordinate, y_coordinate);
+    x = gps_values[0];
+    y = gps_values[2];
+    
+    //printf("Prev GPS: %.3f %.3f\n", x0, y0);
+    //printf("Init GPS: %.3f %.3f\n", x, y);
+    
+    
+      speed_left = MAX_SPEED;
+      speed_right = MAX_SPEED;
 
-    /* compute speed values*/
-    for (i = 0; i < 2; i++) {
-      speed[i] = 0.0;
-      for (j = 0; j < 8; j++)
-        speed[i] += braitenberg_coefficients[j][i] * (1.0 - (sensors_value[j] / RANGE));
-    }
+     for (i = 0; i < 8; i++){
+       /*Obstacle voiding, edit later*/
+      if(sensors_value[i] >= 30){
+        speed_left = 1*MAX_SPEED;
+        speed_right = -0.1* MAX_SPEED;
+      }else{
+        speed_left = MAX_SPEED;
+        speed_right = MAX_SPEED;
+      }
+     }
+     
+     
+    
+
 
     /* set speed values */
-    wb_motor_set_velocity(left_motor, speed[0]);
-    wb_motor_set_velocity(right_motor, speed[1]);
+    wb_motor_set_velocity(left_motor, speed_left);
+    wb_motor_set_velocity(right_motor, speed_right);
 
+    
+    
+    }
+  
+  
+    
     step();
+    
+    if(delta == DELTA){
+    /*record the old coordinates to determine movements and directions*/
+    x0 = x;
+    y0 = y;
+    }
+    
+    
+    
+    delta++;
+
   }
 
   wb_robot_cleanup();
