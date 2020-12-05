3fefa53294e9d5ab8e58b45749880c75f39797b3
Pi <paingthetko.pi@gmail.com>
2020-12-05 20:40:12 +0630

    Update CompletedKUKA.c

--- CompletedKUKA.c
+++ CompletedKUKA.c
@@ -1,10 +1,24 @@
 /*
- * CADT-UH 1038 - Autonomous and Social Robots
- * Simple Pattern Formation of a Robot Swarm
- * KUKA Youbot Controller by Maryam and Pi
- * Description: This controller controls the KUKA Youbot robot to clear the obstacles autonomously
+ * Copyright 1996-2020 Cyberbotics Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
- 
+
+/*
+ * Description:   Starts with a predefined behaviors and then
+ *                read the user keyboard inputs to actuate the
+ *                robot
+ */
 
 #include <webots/keyboard.h>
 #include <webots/robot.h>
@@ -44,7 +58,19 @@ static void automatic_behavior() {
   gripper_grip();
   passive_wait(1.0);
   base_backwards();
-  passive_wait(3.0);
+  passive_wait(4.0);
+  gripper_release();
+  //Grip it again for better grip
+  base_forwards();
+  passive_wait(0.1);
+  gripper_release();
+  arm_set_height(ARM_FRONT_CARDBOARD_BOX);
+  passive_wait(0.5);
+  gripper_grip();
+  passive_wait(1.0);
+  base_backwards();
+  passive_wait(4.0);
+  
 }
 
 
