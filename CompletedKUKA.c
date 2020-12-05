e1595b3b104d5ca0bda1a53d550295f292eff078
Pi <paingthetko.pi@gmail.com>
2020-12-05 21:06:04 +0630

    Update CompletedKUKA.c

--- CompletedKUKA.c
+++ CompletedKUKA.c
@@ -1,24 +1,11 @@
-/*
- * Copyright 1996-2020 Cyberbotics Ltd.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 /*
- * Description:   Starts with a predefined behaviors and then
- *                read the user keyboard inputs to actuate the
- *                robot
+ * CADT-UH 1038 - Autonomous and Social Robots
+ * Simple Pattern Formation of a Robot Swarm
+ * KUKA Youbot Controller by Maryam and Pi
+ * Description: This controller controls KUKA Youbot to remove the obstacles
  */
+ 
 
 #include <webots/keyboard.h>
 #include <webots/robot.h>
